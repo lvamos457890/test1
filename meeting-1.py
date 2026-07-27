3dc5bbcc08b3fd558fb3ff78cd53f7187509180b
lvamos457890 <loviamosy251@gmail.com>
2026-07-27 07:20:59 +0700

    first commit

---
+++ meeting-1.py
@@ -0,0 +1,55 @@
+#print ngeluarin kata kata dan angka ke terminal
+#String untuk memasukan kata kata dan angka
+print("hello world")
+print("nama saya amos saya berumur 13 tahun")
+
+#integer = angka
+print("1"+"1")
+#float = desimal
+print(0,5*0,5)
+#boolean = true or false
+print(True)
+
+# , adalah pemisah antara tipe data
+print("aku punya botol",1)
+print("aku memiliki ",2+1," botol")
+
+#variables adalah wadah untuk menyimpan data
+botol ="air"
+nama = "amos"
+print("hello nama saya",nama)
+
+# perhitungan matematika * / + -
+print(1+1)
+print(2-1)
+print(3*4)
+print(20/5)
+print(4**2)
+print(20%4)
+print((6/10)/(1/6))
+
+# """tes""" untuk sebuah paragraf yang banyak supaya lebih enak dibaca
+print("""Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since 1966, 
+      when designers at Letraset and James Mosley, the librarian at St Bride Printing 
+      Library in London, took a 1914 Cicero translation and scrambled it to make dummy 
+      text for Letraset's Body Type sheets. It has survived not only many decades, 
+      but also the leap into electronic typesetting, remaining essentially unchanged. 
+      It was popularised thanks to these sheets and more recently with desktop publishing 
+      software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.""")
+
+print("#######")
+print("#       #")
+print("#        #")
+print("#        #")
+print("#       #")
+print("########")
+print("# #")
+print("#  #")
+print("#   #")
+print("#    #")
+print("#     #")
+print("#      #")
+
+
+
---
+++ nyoba dari youtbe.py
@@ -0,0 +1,74 @@
+# umur = 13
+# tinggi = 178
+# nama = "amos"
+# print("hallo nama saya " + str(nama) + " umur saya " +str(umur)+ " tinggi saya " + str(tinggi) + "cm")
+
+
+# ada_kayu = True
+# if (ada_kayu):
+#     print ("lompat")
+# else:
+#     print ("jalan terus")
+
+
+
+
+# Doni = 61
+
+# if Doni > 60:
+#     print("Doni harus olahraga")
+# elif Doni == 60:
+#     print("Doni sudah kenyang")
+# else:
+#     print("Doni butuh makan")
+
+# import time
+# Doni = 60
+
+# Doni = int(input("berapa berat badan doni?"))
+
+# print("Sedang menganalisis...")
+# time.sleep(3)
+
+# if Doni > 60:
+#     print("Doni butuh olahraga")
+
+
+
+
+# if Doni == 60:
+#     print("Berat Doni ideal")
+
+
+
+# if Doni < 60:
+#     print("Doni butuh makan")
+
+
+
+
+
+
+
+# i di dalam (for i in range) itu cuma nama bebas aja bebas pake apa aja biasanya tergantung apa yang di promt
+
+
+import time
+
+for i in range(3):
+    time.sleep(1)
+    Doni = int(input("Berapa berat badan Doni? "))
+
+    print("Sedang menganalisis...")
+    time.sleep(3)
+
+    if Doni > 60:
+        print("Doni butuh olahraga")
+
+    elif Doni == 60:
+        print("Berat Doni ideal")
+
+    else:
+        print("Doni butuh makan")
+
+print("END")
